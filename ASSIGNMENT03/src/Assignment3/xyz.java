43423ebb59d894f880cf1c1c06456aa8b887a6cc
User <User@LAPTOP-H55FDBIA>
2024-04-11 14:35:39 -0400

    NEW PROJECT XYZ BY TARUNJIT SINGH

---
+++ ASSIGNMENT03/src/Assignment3/xyz.java
@@ -0,0 +1,23 @@
+package Assignment3;
+
+public class xyz {
+	
+	    public static void main(String[] args) {
+	        int roll = 0;
+	        int[] diceRoll = new int[20];
+	        int totalRolls = 0;
+
+	        for (int count = 0; count < 1000; count++) {
+	            roll = (int) (Math.random() * 20) + 1;
+	            diceRoll[roll - 1] = diceRoll[roll - 1] + 1;
+	        }
+
+	        for (int index = 0; index < diceRoll.length; index++) {
+	            System.out.printf("Count of %d is: %d%n", (index + 1), diceRoll[index]);
+	            totalRolls = totalRolls + diceRoll[index];
+	        }
+	        System.out.println("Total rolls were: " + totalRolls);
+	    }
+	}
+
+
